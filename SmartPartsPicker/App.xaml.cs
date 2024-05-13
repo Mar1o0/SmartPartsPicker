de8545af5e6422190a32e134f234b44ee32a3036
Vladislav Dikevich <vladdikevich@mail.ru>
2024-05-13 21:53:25 +0300

    Добавьте файлы проекта.

---
+++ SmartPartsPicker/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SmartPartsPicker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SmartPartsPicker/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartPartsPicker
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
